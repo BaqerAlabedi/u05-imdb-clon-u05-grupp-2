398edddffed697778dbf6cfcc2ad332cc2fb840f
Viktoria Hillerud <viktoriahillerud@gmail.com>
2023-02-04 15:09:24 +0000

    Made registerpage done kinda

--- Chas-movies/resources/views/register.blade.php
+++ Chas-movies/resources/views/register.blade.php
@@ -79,11 +79,11 @@
             
   <section class="container py-12 px-6 h-96">
     <div class="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
-      <div class="xl:w-10/12">
+      <div class="xl:w-h">
         <div class="block bg-white shadow-lg rounded-lg">
           <div class="mb-10 lg:flex g-0">
-            <div class="lg:w-h px-4 md:px-0">
-              <div class="md:p-12 md:mx-6">
+            <div class="lg:w-h px-4 md:px-0 flex items-stretch">
+              <div class="md:p-12 md:mx-6 my-5">
                 <div class="text-center">
                   <img
                     class="mx-auto w-48"
@@ -99,7 +99,7 @@
                       type="text"
                       class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                       id="exampleFormControlInput1"
-                      placeholder="Username"
+                      placeholder="Select username"
                     />
                   </div>
                   <div class="mb-4">
@@ -107,7 +107,7 @@
                       type="password"
                       class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                       id="exampleFormControlInput1"
-                      placeholder="Password"
+                      placeholder="Select password"
                     />
                   </div>
                   <div class="text-center pt-1 mb-12 pb-1">
@@ -120,9 +120,9 @@
                         background: linear-gradient(to right, rgb(156, 163, 175), rgb(75, 85, 99), rgb(30, 64, 175));
                       "
                     >
-                      Log in
+                      Register
                     </button>
-                    <a class="text-gray-500" href="#!">Forgot password?</a>
+
                   </div>
                   <div class="flex items-center justify-between pb-6">
                     <p class="mb-0 mr-2">Already have an account?</p>
@@ -132,7 +132,7 @@
                       data-mdb-ripple="true"
                       data-mdb-ripple-color="light"
                     >
-                      Login page
+                      Login
                     </button>
                   </div>
                 </form>
