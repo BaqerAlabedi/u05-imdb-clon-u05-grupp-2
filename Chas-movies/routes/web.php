4ed6d8d3c936de7ae8a3bf71089ac81d306a73f5
Viktoria Hillerud <viktoriahillerud@gmail.com>
2023-02-13 11:53:39 +0000

    breeze installed

--- Chas-movies/routes/web.php
+++ Chas-movies/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
 Route::get('/discover', function () {
     return view('discover');
 });
@@ -20,16 +36,7 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/register', function () {
-    return view('register');
-});
-
-
 Route::get('/kategori', function () {
     return view('kategori');
 });
-
-
-Route::get('/login', function () {
-    return view('login');
-});
+require __DIR__.'/auth.php';
