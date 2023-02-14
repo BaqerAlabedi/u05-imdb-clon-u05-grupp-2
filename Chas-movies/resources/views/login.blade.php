f73c9a72df47ee05cc2e82851f113b379ab22b98
Viktoria Hillerud <viktoriahillerud@gmail.com>
2023-02-14 15:34:32 +0000

    more

--- Chas-movies/resources/views/login.blade.php
+++ Chas-movies/resources/views/login.blade.php
@@ -91,7 +91,7 @@
                   />
                   <h4 class="text-xl font-semibold mt-1 mb-12 pb-1">Chas Movies</h4>
                 </div>
-                <form  methos="POST" action="{{ url('login'}}">
+                <form  methos="POST" action="">
                   <p class="mb-4">Please login to your account</p>
                   <div class="mb-4">
                     <input
