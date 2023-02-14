10720cddb1bc32df62f0067a4ee90f1e614b49b6
Viktoria Hillerud <viktoriahillerud@gmail.com>
2023-02-14 12:13:38 +0000

    edited

--- Chas-movies/routes/web.php
+++ Chas-movies/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-//Route::get('/admin', [AdminController::class, 'function']);
+//Route::get('/admin/show-users', [AdminController::class, 'function']);
 
 Route::get('/discover', function () {
     return view('discover');
