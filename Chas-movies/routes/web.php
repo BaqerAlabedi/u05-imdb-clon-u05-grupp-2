188a0ca1a06cafc8a51f8536146fef8b172163e7
samer201001 <samer201001@gmail.com>
2023-02-15 08:40:42 +0100

    breeze

--- Chas-movies/routes/web.php
+++ Chas-movies/routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,47 +14,18 @@ use App\Http\Controllers\UserController;
 |
 */
 
-
-//Route::get('/admin/show-users', [AdminController::class, 'readAllUsers']);
-//Route::post(' ', [UserController::class, 'register']);
-
-Route::post('/login', [UserController::class, 'login']);
-
-Route::get('/discover', function () {
-    return view('discover');
-});
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
-Route::get('/register', function () {
-    return view('register');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/helpcenter', function () {
-    return view('helpcenter');
-});
-
-Route::get('/kategori', function () {
-    return view('kategori');
-});
-
-
-Route::get('/login', function () {
-    return view('login');
-});
-
-Route::get('/film-view', function () {
-    return view('film-view');
-});
-Route::get('/user', function () {
-    return view('user');
-});
-
-Route::get('/admin', function () {
-    return view('admin');
-});
-
-Route::get('/forgot-password', function () {
-    return view('forgotpass');
-});
+require __DIR__.'/auth.php';
