45ab2dd651bbc2e919e51b112ced1ac84d3c037a
Viktoria Hillerud <viktoriahillerud@gmail.com>
2023-02-19 11:22:09 +0000

    edited route

--- Chas-movies/resources/views/dashboard.blade.php
+++ Chas-movies/resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 @if (Auth::user()->role == 1)
 <script>
-    window.location = "{{ url('/movie') }}";
+    window.location = "{{ url('/user/movie') }}";
 </script>
 @endif
 <x-app-layout>
@@ -14,7 +14,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    @if (Auth::user()->role == 0 
+                    @if (Auth::user()->role == 0)
                     {{ __("You're logged in as:") }}
                     <h2>Admin</h2>
 
