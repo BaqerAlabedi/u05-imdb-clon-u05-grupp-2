108ff23f8f176a40e9a1284593b0a139e3d4f836
Viktoria Hillerud <viktoriahillerud@gmail.com>
2023-02-20 07:45:27 +0000

    edited blade files to match navbar

--- Chas-movies/resources/views/user.blade.php
+++ Chas-movies/resources/views/user.blade.php
@@ -36,6 +36,12 @@
             </svg>
             <span class="ml-2 text-sm font-medium">TV-show</span>
           </a>
+          <a class="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="{{ url('/kategori') }}">
+                        <svg class="w-0 h-0 md:w-5 sm:w-5 lg:w-5 md:h-5 sm:h-5 lg:h-5 invisible md:visible lg:visible sm:visible" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-film" viewBox="0 0 16 16">
+                            <path d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V1zm4 0v6h8V1H4zm8 8H4v6h8V9zM1 1v2h2V1H1zm2 3H1v2h2V4zM1 7v2h2V7H1zm2 3H1v2h2v-2zm-2 3v2h2v-2H1zM15 1h-2v2h2V1zm-2 3v2h2V4h-2zm2 3h-2v2h2V7zm-2 3v2h2v-2h-2zm2 3h-2v2h2v-2z" fill="white"></path>
+                        </svg>
+                        <span class="ml-2 text-sm font-medium">Categories</span>
+                    </a>
           <a class="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="{{ url('/trending') }}">
             <span class="w-0 h-0 md:w-5 sm:w-5 lg:w-5 md:h-5 sm:h-5 lg:h-5 invisible md:visible lg:visible sm:visible">🔥</span>
             <span class="ml-2 text-sm font-medium">Trending</span>
