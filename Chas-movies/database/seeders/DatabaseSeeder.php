7c33bfa464291bf584f6c46c68855a01779be110
Viktoria Hillerud <viktoriahillerud@gmail.com>
2023-02-22 14:49:35 +0000

    Added seeding possibility on branch "seeders"

--- Chas-movies/database/seeders/DatabaseSeeder.php
+++ Chas-movies/database/seeders/DatabaseSeeder.php
@@ -14,11 +14,49 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+         'email' => 'test@example.com',
+     ]);
+
+
+     \App\Models\Film::factory()->create([
+        'title' => 'Avatar 2',
+       'genre' => 'Fantasy',
+        'director' => 'nån',
+        'maincast' => 'några',
+        'imgurl' => 'https://m.media-amazon.com/images/M/MV5BZDA0OGQxNTItMDZkMC00N2UyLTg3MzMtYTJmNjg3Nzk5MzRiXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_.jpg',
+    ]);
+
+    \App\Models\Film::factory()->create([
+        'title' => 'Memory',
+       'genre' => 'Action',
+        'director' => 'nån',
+        'maincast' => 'Liam Neeson',
+        'imgurl' => 'https://m.media-amazon.com/images/M/MV5BOGI5N2FhNzktZjZlNi00MmRjLWE1MmUtNjRlNzQyOGMzYjNhXkEyXkFqcGdeQXVyMDA4NzMyOA@@._V1_FMjpg_UX1000_.jpg',
+    ]);
+
+    \App\Models\Show::factory()->create([
+        'title' => 'Scrubs',
+       'genre' => 'Drama',
+        'director' => 'nån',
+        'maincast' => 'Liam Neeson',
+        'seasons' => 11, 
+        'episodes' => 234,
+        'imgurl' => 'https://m.media-amazon.com/images/M/MV5BOGI5N2FhNzktZjZlNi00MmRjLWE1MmUtNjRlNzQyOGMzYjNhXkEyXkFqcGdeQXVyMDA4NzMyOA@@._V1_FMjpg_UX1000_.jpg',
+    ]);
+
+    \App\Models\Show::factory()->create([
+        'title' => 'Something',
+       'genre' => 'Action',
+        'director' => 'nån',
+        'maincast' => 'Liam Neeson',
+        'seasons' => 2, 
+        'episodes' => 32,
+        'imgurl' => 'https://m.media-amazon.com/images/M/MV5BOGI5N2FhNzktZjZlNi00MmRjLWE1MmUtNjRlNzQyOGMzYjNhXkEyXkFqcGdeQXVyMDA4NzMyOA@@._V1_FMjpg_UX1000_.jpg',
+    ]);
+
     }
 }
